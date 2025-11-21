fecd63cf3f5567ad92efe0d7899ae1de043d2fdb
Maps67 <marcoaps67@hotmail.com>
2025-11-20 19:55:56 -0600

    Fix: Downgrade a Tailwind v3 estable para corregir build

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
-  // INCRUSTAMOS LA CONFIGURACIÓN CSS AQUÍ
-  css: {
-    postcss: {
-      plugins: [
-        tailwindcss(),
-        autoprefixer(),
-      ],
-    },
-  },
   plugins: [
     react(),
     VitePWA({
