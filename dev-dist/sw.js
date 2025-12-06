8ca6e6026670854435f61be04f2ce39c6f43c9b6
Maps67 <marcoaps67@hotmail.com>
2025-12-06 16:17:34 -0600

    FEAT: Nuevo diseño profesional de Receta PDF y correcciones de UI

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.enb4ja7q0sg"
+    "revision": "0.k3ih5hnppa"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
